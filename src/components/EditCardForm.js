72809860d501b0de34a00b501b5669224d96b5ce
Chelsea Becker <63199870+cschweig2@users.noreply.github.com>
2021-02-22 15:15:07 -0800

    add update functionality w. firestore
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- src/components/EditCardForm.js
+++ src/components/EditCardForm.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
+import { useFirestore } from 'react-redux-firebase';
 
 function EditCardForm (props) {
+  const firestore = useFirestore();
   const { card } = props;
 
   function handleEditCardFormSubmission(event) {
     event.preventDefault();
-    props.onEditCard({
+    props.onEditCard();
+    const propertiesToUpdate = {
       term: event.target.term.value,
-      definition: event.target.definition.value,
-      id: card.id});
+      definition: event.target.definition.value
+    }
+    return firestore.update({collection: 'cards', doc: card.id}, propertiesToUpdate)
   }
 
   return (
